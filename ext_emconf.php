9b24b3d7c80f399ba5256de02f2ebe783b3ebcc2
Tomasz Woldański <t.woldanski@macopedia.com>
2023-11-14 11:20:10 +0100

    [TASK] Add support for T3 v12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,12 +4,12 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache Purger',
     'description' => 'Purge cached URLs within Varnish instances',
     'category' => 'misc',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
